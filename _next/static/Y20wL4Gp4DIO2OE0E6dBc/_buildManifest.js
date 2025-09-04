e8a294b235c140c549cc0c9e04421c36afdbd975
Abhinaya <abhinaya.sukumar@gmail.com>
2025-09-04 13:41:57 +0400

    Updates

---
+++ _next/static/Y20wL4Gp4DIO2OE0E6dBc/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/449d2df5b678e4f1.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
