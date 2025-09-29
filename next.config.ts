9452dd088a2feb8c5086b1c57a6833cc44be670e
Senthil  V S <senthil@rippling.com>
2025-09-29 16:50:56 +0400

    fix redirects

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  images: { unoptimized: true },
+  async redirects() {
+    return [
+      // /presentations/foo  →  /presentations/foo/
+      // (the trailing slash will serve .../index.html from /public)
+      {
+        source: '/presentations/:id',
+        destination: '/presentations/:id/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
